7ae9c353b0abf27a1d0a2a97672da49dd8b93fab
EduardoBlanco5 <eduardo.blanco0841@alumnos.udg.mx>
2025-01-08 22:35:33 -0600

    hicismos cambios en el tes.js

--- model/test.js
+++ model/test.js
@@ -3,8 +3,6 @@ import express from "express";
 import { PORT } from "./config.js";
 import path from 'path';
 import { fileURLToPath } from 'url';  
-import http from 'http';  // Necesario para crear un servidor HTTP
-import { Server } from 'socket.io';  // Para crear el servidor de WebSockets
 import dotenv from 'dotenv';
 import router from './routes/test.routes.js';
 import { db } from './db.js';
@@ -13,30 +11,6 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 
 dotenv.config();
-
-// Crear el servidor HTTP
-const server = http.createServer(app);
-
-// Crear el servidor Socket.IO usando el servidor HTTP
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:5173',  // Permitir conexiones desde el frontend (puerto 5173)
-    methods: ['GET', 'POST'],
-  }
-});
-
-// Configurar los eventos de Socket.IO
-io.on('connection', (socket) => {
-    console.log('Un cliente se ha conectado');
-    
-    socket.on('assignChallenge', (data) => {
-      io.emit('newChallenge', data);  // Enviar la notificación a todos los clientes
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('Un cliente se ha desconectado');
-    });
-});
 
 // Obtener el directorio actual
 const __filename = fileURLToPath(import.meta.url);
@@ -54,11 +28,7 @@ app.use(express.static(path.join(__dirname, '../uploads/Tasks')));
 app.use(express.static(path.join(__dirname, '../Teachers/Tasks')));
 
 // Configuración de CORS para el backend (API)
-app.use(cors({
-  origin: 'http://localhost:5173',  // Permitir solicitudes desde el frontend (puerto 5173)
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/', router);
@@ -78,9 +48,4 @@ app.get('/', (req, res) => {
 // Puerto de prueba para ver funcionamiento
 app.listen(PORT, () => {
   console.log(`Servidor de la API en http://localhost:${PORT}`);
-});
-
-// Iniciar el servidor HTTP para Socket.IO en el puerto 7000
-server.listen(7000, () => {
-    console.log('Servidor de WebSockets corriendo en http://localhost:7000');
 });
